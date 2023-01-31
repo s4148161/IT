bceca9858ba5cddb9bc32851e346a1501809b346
Callum McMahon <s4148161@terrace.qld.edu.au>
2023-01-31 11:50:23 +1000

    intital

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+alert("Max Mackley is a bum")
+
+numberOfApples = prompt("How many apples?");
+numberOfApples = Number(numberOfApples);
+cost = 1;
+message = "Your total cost is";
+shopclosed = false;
+
+total = cost * numberOfApples;
+alert(message + String(total));
